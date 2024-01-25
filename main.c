667f3c3b3a9292458e87acbeec34d1ed6f76cb80
Tomasz Buklaho <flyff2234@gmail.com>
2024-01-25 01:25:43 +0100

    final

--- main.c
+++ main.c
@@ -6,30 +6,16 @@
 #define and &&
 #define or ||
 
-#if defined(_WIN32) || defined(_WIN64)
-    #define WINDOWS
-#elif defined(_linux_) || defined(linux)
-    #define LINUX
-#elif defined(__APPLE__) || defined(_APPLE_) || defined(__MACH__)
-    #define MACOS
-#else
-    #define OTHER_OS
-#endif
-
 float* pieniadze;
 
-void clearconsole(){
-    #ifdef WINDOWS
-        system("cls");
-    #elif defined(LINUX) || defined(MACOS)
-        printf("\033[1;1H\033[2J");
-    #endif
+void clearconsole() {
+    system("cls");
 }
 
 int safescanf() {
     int zmienna;
     while (scanf("%d", &zmienna) != 1) {
-        printf("Błędne dane. Podaj liczbę ponownie: ");
+        printf("Bledne dane. Podaj liczbe ponownie: ");
         while (getchar() != '\n');
     }
     return zmienna;
@@ -76,11 +62,11 @@ Element* wczytaj_pytania(Element* head, char* nazwa_pliku, int* liczba_pytan_pli
     }
     int max_rozmiar = 600;
     char *bufer = (char*)malloc(sizeof(char)*max_rozmiar);
-    
+
     //biore linie, tworze na jej podstawie pytanie, dodaje pytanie do listy
     while(fgets(bufer, max_rozmiar, plik)){
         Pytanie pytanie;
-        
+
         int k=0, i=0;
         while(bufer[k] != ':'){
             pytanie.tresc_pytania[i] = bufer[k];
@@ -109,7 +95,7 @@ Element* wczytaj_pytania(Element* head, char* nazwa_pliku, int* liczba_pytan_pli
 
     free(bufer);
     fclose(plik);
-    
+
     return head;
 }
 
@@ -159,6 +145,10 @@ Element* losuj_pytania(Element* head, int liczba_pytan_plik){
         new_head = dodaj_do_listy(new_head, temp->pytanie);
         head = usun_indeks(head, j);
     }
+
+    for(int i=liczba_pytan; i<liczba_pytan_plik; i++){
+        head = usun_indeks(head, 0);
+    }
     return new_head;
 }
 
@@ -205,7 +195,7 @@ void dodaj_pytanie_do_pliku(char* nazwa_pliku, char* pytanie, char* odp1, char* 
     usun_znak(odp2, '\n');
     usun_znak(odp3, '\n');
     usun_znak(odp4, '\n');
-    
+
     FILE *plik = fopen(nazwa_pliku, "a");
     if(plik == NULL){
         printf("Nie mozna otworzyc pliku z pytaniami");
@@ -307,9 +297,9 @@ float mnoznik(int liczba){
 
 void pisz_decyzja_gra(){
     printf("Wybierz jedno: \n");
-        printf("1. Chce zaznaczyc odpowiedz.\n");
-        printf("2. Chce uzyc kola ratunkowego.\n");
-        printf("3. Chce zrezygnowac z dalszej gry.\n");
+    printf("1. Chce zaznaczyc odpowiedz.\n");
+    printf("2. Chce uzyc kola ratunkowego.\n");
+    printf("3. Chce zrezygnowac z dalszej gry.\n");
 }
 
 void czy_przegrano(int wartosc, int runda){
@@ -317,6 +307,7 @@ void czy_przegrano(int wartosc, int runda){
     if(wartosc==1){
         printf("Bardzo nam przykro.\n");
         printf("Odchodzisz z niczym :(\n");
+        sleep(2);
     }
     if(wartosc==2){
         if(runda!=0)
@@ -325,10 +316,14 @@ void czy_przegrano(int wartosc, int runda){
             printf("Niestety, ale nic nie wygrales\n");
             printf("Mogles chociaz sprobowac, nastepnym razem sie uda!\n");
         }
+        sleep(3);
     }
-    sleep(3);
+    if(wartosc==4){
+        printf("Wyswietle szczegoly rundy raz jeszcze:\n");
+        sleep(1);
+    }
     clearconsole();
-    
+
 }
 
 void informacje_i_tresc_pytania(Element* head, int i){
@@ -378,11 +373,25 @@ void odpowiedni_komunikat_po_rundzie(int wynik,int i){
     }
     else if(!wynik){
         clearconsole();
-        printf("Gratulacje, to poprawna odpowiedz!\n");
-        printf("Twoja obecna wygrana wynosi: %d zl\n",(int)pieniadze[i+1]);
-        printf("Przechodzimy do pytania nr %d\n",i+2);
-        sleep(4);
-        clearconsole();
+        if(i+2!=13){
+            printf("Gratulacje, to poprawna odpowiedz!\n");
+            printf("Twoja obecna wygrana wynosi: %d zl\n",(int)pieniadze[i+1]);
+            printf("Przechodzimy do pytania nr %d\n",i+2);
+            sleep(4);
+            clearconsole();
+        }
+        else{
+            sleep(1);
+            printf("GRATULACJE, wygrywasz MILION ZLOTYCH!\n");
+            sleep(1);
+            printf("Jestes teraz PRAWDZIWYM MILIONEREM!\n");
+            sleep(1);
+            printf("Jeszcze raz gratulujemy sukcesu i zapraszamy do czestszej gry!\n\n");
+            sleep(2);
+            printf(":)\n");
+            sleep(1);
+            clearconsole();
+        }
     }
 }
 
@@ -408,6 +417,8 @@ typedef struct pair{
     int odp2;
 } pair;
 
+pair wylos_odp;
+
 pair fifty_fifty(int poprawna, Element* head){
     srand(time(0));
     int druga_odp = rand()%4+1;
@@ -419,132 +430,215 @@ pair fifty_fifty(int poprawna, Element* head){
         druga_odp = poprawna;
         poprawna = pom;
     }
-    clearconsole();
     printf("Pytanie  brzmialo:\n");
     printf("%s\n",head->pytanie.tresc_pytania);
-    printf("Odrzucilem dwie niepoprawne odpowiedzi.\n");
-    printf("Jedna z poprawnych odpowiedzi to: \n");
-    char litera1 = druga_odp + 'a' - 1;
-    char litera2 = poprawna + 'a' - 1;
-    printf("%c - %s\nlub\n%c - %s\n",litera1, head->pytanie.mozliwe_odpowiedzi[druga_odp-1], litera2,head->pytanie.mozliwe_odpowiedzi[poprawna-1]);
-    
+    printf("ODRZUCILEM dwie NIEPOPRAWNE odpowiedzi.\n");
+    printf("Jedna z POPRAWNYCH odpowiedzi to: \n");
+    char litera1 = druga_odp + 'A' - 1;
+    char litera2 = poprawna + 'A' - 1;
+    printf("\n%c - %s\nlub\n%c - %s\n\n",litera1, head->pytanie.mozliwe_odpowiedzi[druga_odp-1], litera2,head->pytanie.mozliwe_odpowiedzi[poprawna-1]);
+
     pair X;
     X.odp1 = druga_odp-1;
     X.odp2 = poprawna-1;
     return X;
 }
 
-void kola_ratunkowe(Element* head, int** ratunek, int* wynik){
-    int kolo;
-    int decyzja2 = -1;
-    int liczba_dostepnych_kol = ((*ratunek)[0]) + ((*ratunek)[1]) + ((*ratunek)[2]);
-    pair wylos_odp;
-    wylos_odp.odp1=-1;
-    wylos_odp.odp2=-1;
-    
-    if(liczba_dostepnych_kol == 0){
-        printf("\nWykorzystales wszystkie kola ratunkowe\n");
-        printf("Wybierz '1' by zaznaczyc odpowiedz.\n");
-        printf("Wybierz inna cyfre by zrezygnowac z dalszej gry.\n");
-        decyzja2=safescanf();
-        if(decyzja2==1){
-            printf("Podaj jedna z odpowiedzi a-d");
-            if(wczytaj_i_sprawdz_odpowiedz(head)){
-                *wynik = 0;
-            }
-            else{
-                *wynik = 1;
-                komunikat_zla_odp(head);
-            }
+void telefon_do_przyjaciela(Element* head, pair X,int runda){
+    int poprawna = head->pytanie.poprawna_odpowiedz - 1;
+    int losuj = rand()%100+runda;
+    int odpowiedz_przyjaciela;
+    if(X.odp1==-1 and X.odp1==-1){
+        if(losuj<80){
+            odpowiedz_przyjaciela = poprawna;
         }
         else{
-            *wynik = 2;
+            do{
+                odpowiedz_przyjaciela = rand()%4;
+            }while(odpowiedz_przyjaciela==poprawna);
         }
     }
-    while(liczba_dostepnych_kol != 0){
-        printf("Ktorego z kol chcesz uzyc? \n");
-        printf("1. Pytanie do Publicznosci\n");
-        printf("2. 50/50\n");
-        printf("3. Telefon do przyjaciela\n");
-        kolo = -1;
-        while(kolo<1 or kolo>3){
-            printf("Wybierz [1-3]");
-            kolo = safescanf();
-            if(kolo<1 or kolo>3){
-                printf("Podaj liczbe z przedzialu 1-3.\n");
+    else{
+        if(losuj<90){
+            odpowiedz_przyjaciela = poprawna;
+        }
+        else{
+            if(poprawna == X.odp1){
+                odpowiedz_przyjaciela = X.odp2;
             }
-            if((*ratunek)[kolo-1] == 0){
-                printf("Uzyles juz danego kola!\n");
-                break;
+            else{
+                odpowiedz_przyjaciela = X.odp1;
             }
-            if(kolo==1){
-                // UZUPELNIC
-                printf("Pytanie do publicznosci\n");
+
+        }
+
+    }
+    printf("\nTWOJ PRZYJACIEL na Twoim miejscu WYBRALBY ODPOWIEDZ: %c\n",odpowiedz_przyjaciela+'A');
+}
+
+void pytanie_publicznosc(Element* head,pair X){
+    srand(time(0));
+    int poprawna = head->pytanie.poprawna_odpowiedz-1;
+    if(X.odp1==-1 and X.odp2==-1){
+        int *glosowanie = (int*)malloc(4*sizeof(int));
+        glosowanie[poprawna] = 20;
+        for(int i=0;i<80;i++){
+            int pojedynczy_glos = rand()%4;
+            glosowanie[pojedynczy_glos]++;
+        }
+        sleep(1);
+        printf("Tak prezentuja sie glosy publicznosci: \n");
+        printf("ODPOWIEDZ A: %d %%\n",glosowanie[0]);
+        printf("ODPOWIEDZ B: %d %%\n",glosowanie[1]);
+        printf("ODPOWIEDZ C: %d %%\n",glosowanie[2]);
+        printf("ODPOWIEDZ D: %d %%\n",glosowanie[3]);
+        free(glosowanie);
+    }
+    else{
+        int ile_poprawna = 30;
+        int ile_zla = 0;
+        for(int i=0;i<70;i++){
+            int pojedynczy_glos = rand()%2;
+            if(!pojedynczy_glos){
+                ile_poprawna++;
             }
-            if(kolo==2){
-                wylos_odp = fifty_fifty(head->pytanie.poprawna_odpowiedz,head);
-            }
-            if(kolo==3){
-                // UZUPELNIC
-                printf("Telefon do przyjaciela\n");
+            else{
+                ile_zla++;
             }
         }
-        (*ratunek)[kolo-1] = 0;
-        liczba_dostepnych_kol = ((*ratunek)[0]) + ((*ratunek)[1]) + ((*ratunek)[2]);
-        if(liczba_dostepnych_kol == 0){
+        printf("Tak prezentuja sie glosy publicznosci: \n");
+        char literka1 = X.odp1 + 'A';
+        char literka2 = X.odp2 + 'A';
+
+        int liczba1,liczba2;
+        if(poprawna  == (int)(literka1 - 'A')){
+            liczba1 = ile_poprawna;
+            liczba2 = ile_zla;
+        }
+        else{
+            liczba1 = ile_zla;
+            liczba2 = ile_poprawna;
+        }
+        printf("ODPOWIEDZ %c: %d %%\n",literka1,liczba1);
+        printf("ODPOWIEDZ %c: %d %%\n",literka2,liczba2);
+    }
+}
+
+void WyswietlPonownie(Element* head, int runda){
+    clearconsole();
+    printf("Przypominam, ze walczysz teraz o %d zl!\n",(int)pieniadze[runda+1]);
+    printf("Wyswietlam ponownie pytanie:\n");
+
+    printf("%s\n",head->pytanie.tresc_pytania);
+    printf("Tak prezentuja sie odpowiedzi:\n\n");
+    for(int i=0;i<4;i++){
+        printf("Odpowiedz %c: %s\n",'a'+i,head->pytanie.mozliwe_odpowiedzi[i]);
+    }
+    printf("\n");
+}
+
+void kola_ratunkowe(Element* head, int** ratunek, int* wynik, int runda){
+    int kolo=-1;
+    int decyzja2=-1;
+    int liczba_dostepnych_kol = ((*ratunek)[0]) + ((*ratunek)[1]) + ((*ratunek)[2]);
+
+
+    if(liczba_dostepnych_kol == 0){
+        do{
             printf("\nWykorzystales wszystkie kola ratunkowe\n");
             printf("Wybierz '1' by zaznaczyc odpowiedz.\n");
-            printf("Wybierz inna cyfre by zrezygnowac z dalszej gry.\n");
+            printf("Wybierz '3' by zrezygnowac z dalszej gry.\n");
             decyzja2=safescanf();
             if(decyzja2==1){
-                printf("Podaj jedna z odpowiedzi a-d");
+                WyswietlPonownie(head, runda);
+                printf("Podaj jedna z odpowiedzi a-d:");
                 if(wczytaj_i_sprawdz_odpowiedz(head)){
                     *wynik = 0;
+                    return;
                 }
                 else{
                     *wynik = 1;
                     komunikat_zla_odp(head);
+                    return;
                 }
             }
-            else{
+            else if(decyzja2==3){
                 *wynik = 2;
+                return;
             }
-            break;
-        }
-        
-        printf("Wybierz '1' by zaznaczyc odpowiedz.\n");
-        printf("Wybierz '2' by zrezygnowac z dalszej gry.\n");
-        printf("Wybierz inna cyfre aby przejsc do menu wyboru kolejego kola.\n");
-        decyzja2=safescanf();
-        if(decyzja2==1){
-            printf("Podaj jedna z odpowiedzi a-d");
-            if(wczytaj_i_sprawdz_odpowiedz(head)){
-                *wynik = 0;
+        } while(decyzja2!=1 and decyzja2!=3);
+
+    }
+    if(liczba_dostepnych_kol != 0){
+        printf("Wybierz jedno z kol lub Podejmij dalsze dzialanie(4): \n");
+        printf("1. Pytanie do Publicznosci\n");
+        printf("2. 50/50\n");
+        printf("3. Telefon do przyjaciela\n");
+        printf("4. ZAZNACZ ODPOWIEDZ lub ZREZYGNUJ Z GRY\n");
+        do{
+            printf("Wybierz [1-4]\n");
+            kolo = safescanf();
+            if(kolo<1 or kolo>4){
+                printf("Podaj liczbe z przedzialu 1-4.\n");
             }
-            else{
-                *wynik = 1;
-                komunikat_zla_odp(head);
+            if(kolo!=4 and (*ratunek)[kolo-1] == 0){
+                printf("Uzyles juz danego kola!\n");
+                *wynik=4;
+                sleep(1);
+                return;
             }
-            break;
-        }
-        else if(decyzja2==2){
-            *wynik = 2;
-            break;
-        }
+            else if(kolo==1){
+                printf("\nUzywasz kola: Pytanie do publicznosci\n\n");
+                sleep(1);
+                (*ratunek)[kolo-1]--;
+                pytanie_publicznosc(head,wylos_odp);
+                kola_ratunkowe(head,ratunek,wynik,runda);
+                return;
+            }
+            if(kolo==2){
+                printf("\nUzywasz kola: 50/50\n\n");
+                sleep(1);
+                wylos_odp = fifty_fifty(head->pytanie.poprawna_odpowiedz,head);
+                (*ratunek)[kolo-1]--;
+                kola_ratunkowe(head,ratunek,wynik,runda);
+                return;
+            }
+            if(kolo==3){
+                printf("\nUzywasz kola: Telefon do przyjaciela\n\n");
+                sleep(1);
+                (*ratunek)[kolo-1]--;
+                telefon_do_przyjaciela(head,wylos_odp,runda);
+                kola_ratunkowe(head,ratunek,wynik,runda);
+                return;
+            }
+            if(kolo==4){
+                *wynik=4;
+                return;
+            }
+        } while(kolo<1 and kolo>4);
+
     }
 }
 
 void play(Element* head){
     Zasady_i_akceptacja_regulaminu();
     int* ratunek = (int*)malloc(3*sizeof(int));
+    // Ponizej mozna zmodyfikowac ilosc dostepnych uzyc danych kol ratunkowych
+    // ratunek[0] <- Pytanie do publicznosci
+    // ratunek [1] <- 50/50
+    // ratunek [2] <- Telefon do przyjaciela
     ratunek[0]=1;
     ratunek[1]=1;
     ratunek[2]=1;
-    int wynik = -1;
-    for(int i=0;i<12;i++){ 
+
+    wylos_odp.odp1=-1;
+    wylos_odp.odp2=-1;
+
+    for(int i=0;i<12;i++){
+        int wynik=-1;
         informacje_i_tresc_pytania(head,i);
-        int decyzja = -1;
-        while(decyzja!=1 and decyzja!=2 and decyzja!=3){
+        int decyzja;
+        do {
             pisz_decyzja_gra();
             decyzja = safescanf();
             if(decyzja==1){
@@ -557,33 +651,11 @@ void play(Element* head){
                     komunikat_zla_odp(head);
                 }
             }
+
             else if(decyzja==2){
-                /*if(ratunek[1]){
-                    fifty_fifty(head->pytanie.poprawna_odpowiedz); 
-                    ratunek[1]=0;                
-                }
-                else{
-                    printf("Wykorzystales to kolo ratunkowe!\n");
-                }
-                printf("Chce:\n");
-                printf("Wybierz '1' by zaznaczyc odpowiedz.\n");
-                printf("Wybierz inna liczbe by zrezygnowac z dalszej gry.\n");
-                int decyzja2=safescanf();
-                if(decyzja2==1){
-                    printf("Podaj jedna z odpowiedzi a-d");
-                    if(wczytaj_i_sprawdz_odpowiedz(head)){
-                        wynik = 0;
-                    }
-                    else{
-                        wynik = 1;
-                        komunikat_zla_odp(head);
-                    }                   
-                }
-                else{
-                    wynik = 2;
-                } */
-                kola_ratunkowe(head, &ratunek, &wynik);
+                kola_ratunkowe(head, &ratunek, &wynik,i);
             }
+
             else if(decyzja==3){
                 wynik = 2;
                 komunikat_rezygnacja(head);
@@ -591,13 +663,23 @@ void play(Element* head){
             else{
                 printf("Podaj liczbe z przedzialu 1-3.\n");
             }
-        }
+        } while(decyzja!=1 and decyzja!=2 and decyzja!=3);
+
+
         odpowiedni_komunikat_po_rundzie(wynik,i);
-        if(wynik){
+        if(wynik and wynik!=4){
             break;
         }
-        head = head -> next;
+        else if (wynik==4){
+            i--;
+        }
+        else{
+            wylos_odp.odp1 = -1;
+            wylos_odp.odp2 = -1;
+            head = head -> next;
+        }
     }
+
     free(ratunek);
 }
 
@@ -609,17 +691,19 @@ int main(){
     Element* head = NULL;
     generuj_pieniadze();
     while(1){
+        clearconsole();
         wyswietl_menu();
         int wybor;
         wybor = safescanf();
         switch(wybor){
-            case 1: 
+            case 1:
                 //printf("%d\n",liczba_pytan_plik);
                 liczba_pytan_plik = 0;
                 head = wczytaj_pytania(head, nazwa_pliku, &liczba_pytan_plik);
                 head = losuj_pytania(head, liczba_pytan_plik);
                 //printf("%d\n",liczba_pytan_plik);
                 printf("Rozpocznijmy gre!\n");
+                //wypisz_liste(head);
                 play(head);
                 break;
             case 2:
@@ -643,10 +727,11 @@ int main(){
                 printf("Wpisz tresc czwartej odpowiedzi\n");
                 fgets(odp4_uzytkownika, max_rozmiar/2, stdin);
 
-                // tutaj czytam inta w niebezpieczny sposob :)
-                int poprawna_uzytkownika = 0;
-                printf("Podaj numer poprawnej odpowiedzi [1 - 4]\n");
-                scanf("%d", &poprawna_uzytkownika);
+                int poprawna_uzytkownika = -1;
+                while(poprawna_uzytkownika<1 or poprawna_uzytkownika>4){
+                    printf("Podaj numer poprawnej odpowiedzi [1 - 4]\n");
+                    poprawna_uzytkownika = safescanf();
+                }
 
                 dodaj_pytanie_do_pliku(nazwa_pliku, pytanie_uzytkownika, odp1_uzytkownika, odp2_uzytkownika, odp3_uzytkownika, odp4_uzytkownika, poprawna_uzytkownika);
 
@@ -660,8 +745,7 @@ int main(){
                 przywroc_fabryczne_w_r(nazwa_pliku,nazwa_fabryczna);
                 clearconsole();
                 printf("Przywrocono ustawienia fabryczne.\n");
-                sleep(1);
-                printf("W puli znajduje sie teraz %d pytan.\n", liczba_pytan_plik);
+                sleep(2);
                 break;
             case 4:
                 clearconsole();
@@ -672,7 +756,7 @@ int main(){
                 printf("Nie ma takiej opcji! Sprobuj jeszcze raz!\n");
                 break;
         }
-    }  
+    }
 
     //fajnie byloby losuj_pytania zapisac do innej zmiennej, zeby nie zgubic heada i zwolnic pamiec
     //head = losuj_pytania(head);
